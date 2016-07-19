490bcae34117356583cb5374370f6c189f2ff474
Oscar Perez <yosoyoscar@gmail.com>
2016-07-19 23:05:48 +0200

    final first version

--- www/js/app.js
+++ www/js/app.js
@@ -72,6 +72,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.friends', 's
       }
     })
 
+    .state('app.about', {
+      url: '/about',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/about.html',
+          controller: ''
+        }
+      }
+    })
+
     ;
 
   // if none of the above states are matched, use this as the fallback
